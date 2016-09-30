7e0697ea584b21287451bd54bb10e5920ec45b1e
Laure <laure.milian@gmail.com>
2016-09-30 14:02:08 +0200

    Ajout messages aléatoires tous les 20 clics

--- app.js
+++ app.js
@@ -2,21 +2,25 @@ $(document).ready(function(){
 
 	counter = $("span").val();
 
-	var messages = ["message a", "message b", "message c"];
+	var messages = ["Allez faire autre chose", "Il fait beau dehors", "Courage fuyons", "Le fond de l'air est frais"];
 
-	var len = messages.length;
+	function getRandomIntInclusive(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min +1)) + min;
+	}
 
 	$(".cake").click(function() {
 		counter++;
 		$("span").html(counter);
 
 		if (counter % 20 === 0) {
-			alert("Déja " + counter + " clics ! Allez faire autre chose. Svp. Si vous voulez bien. Faire la vaisselle. Peut-être. Désolée. Je veux pas insister non plus.");
-			var i = 0;
-			console.log(messages[i]);
-	
+			var i = getRandomIntInclusive(1, 3);
+			alert(messages[i]);
 		}
 
 	});
 
 });
+
+
