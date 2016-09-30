4106c862b49204235d4e67bbb5e6b2880021caaf
Laure <laure.milian@gmail.com>
2016-09-30 12:35:09 +0200

    Click ok + message - pas encore aléatoire

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-	//votre code
 
+	counter = $("span").val();
+
+	var messages = ["message a", "message b", "message c"];
+
+	var len = messages.length;
+
+	$(".cake").click(function() {
+		counter++;
+		$("span").html(counter);
+
+		if (counter % 20 === 0) {
+			alert("Déja " + counter + " clics ! Allez faire autre chose. Svp. Si vous voulez bien. Faire la vaisselle. Peut-être. Désolée. Je veux pas insister non plus.");
+			var i = 0;
+			console.log(messages[i]);
+	
+		}
+
+	});
 
 });
